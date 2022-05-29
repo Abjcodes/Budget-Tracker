ee901d9f2599fa16d38b62df00b3299098b4d262
unknown <abijithmv01@gmail.com>
2022-05-29 09:48:35 +0530

    Added base html and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Stack } from "react-bootstrap";
+import BudgetCard from "./components/BudgetCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <Container className="my-4">
+    <Stack direction="horizontal" gap = "2" className="mb-4">
+      <h1 className="me-auto">Budget Tracker</h1>
+      <Button variant = "success">Add Budget</Button>
+      <Button variant = "outline-success">Add Expense</Button>
+    </Stack>
+    <div style={{ display:"grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", 
+    gap:"1rem",
+    alignItems: "flex-start"}}>
+      <BudgetCard name={"Food"} amount= {500} max={1000} grey></BudgetCard>
     </div>
-  );
+  </Container>
 }
-
 export default App;
---
+++ src/components/BudgetCard.js
@@ -0,0 +1,47 @@
+import { Button, Card, ProgressBar, Stack } from "react-bootstrap";
+import { currencyFormatter } from "../utils";
+
+export default function BudgetCard({name, max, amount, grey}) {
+
+    // For setting the background color of the cards 
+    const classNames = []
+    if(amount>max) {
+        //To clear the array for better memory utilization
+        classNames.length = 0
+        classNames.push("bg-danger", "bg-opacity-10")
+    } else if(grey) {
+        classNames.length = 0
+        classNames.push("bg-light")
+    }
+
+  return (
+        <Card className= {classNames.join(" ")}>
+            <Card.Body>
+                <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
+                    <div className="me-2">{name}</div>
+                    <div className="d-flex align-items-baseline">
+                        {currencyFormatter.format(amount)} /
+                        <span className="text-muted fs-6 ms-1">{currencyFormatter.format(max)}</span></div>
+                </Card.Title>
+                <ProgressBar className="rounded-pill" variant={setVariant(amount, max)}
+                min = {0}
+                max = {max}
+                now = {amount}/>
+                <Stack direction="horizontal" gap = "2" className="mt-4">
+                <Button variant = "success" className="ms-auto">Add Expense</Button>
+                <Button variant = "outline-primary">View Expenses</Button>
+                </Stack>
+            </Card.Body>
+        </Card>
+  )
+}
+
+// For setting the colors of progress bar
+function setVariant(amount, max) {
+    const ratio = amount/max
+    if(ratio < .5 ) return "primary"
+    if(ratio < .75 ) return "warning"
+    return "danger"
+    
+
+}
